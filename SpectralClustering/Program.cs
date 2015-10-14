5424c3a863eaa2830d100e31e9dff469d6a37029
Lasse Vang Gravesen <lgrave11@student.aau.dk>
2015-10-14 09:19:20 +0200

    Clean up

--- SpectralClustering/Program.cs
+++ SpectralClustering/Program.cs
@@ -39,7 +39,6 @@ namespace SpectralClustering
             }
             var r = new Random();
             lp = lp.OrderBy(x => r.Next()).ToList();
-            //lp = lp.OrderBy(x => x.y).ToList();
             List<List<Double>> S = new List<List<double>>();
             for (int i = 0; i < lp.Count; i++)
             {
@@ -62,21 +61,6 @@ namespace SpectralClustering
                 S.Add(row);
             }
 
-            /*for(int x=0; x < S.Count; x++)
-            {
-                for (int y = 0; y < S[x].Count; y++)
-                {
-                    lp.points.
-                }
-            }*/
-
-            /*Matrix<double> Abla = DelimitedReader.Read<double>("cluster6.txt", false, ",", false);
-            List<Point> lp2 = new List<Point>();
-            for (int x = 0; x < Abla.RowCount; x++)
-            {
-                lp2.Add(new Point(x, 0));
-            }*/
-
             var asm = S.Select(Enumerable.ToArray).ToArray();
             double[,] asm2 = new double[asm.Length, asm.Max(x => x.Length)];
             for (var i = 0; i < asm.Length; i++)
@@ -87,52 +71,6 @@ namespace SpectralClustering
                 }
             }
             Matrix<double> A = Matrix<double>.Build.DenseOfArray(asm2);
-            /*for (int x = 0; x < S.Count; x++)
-            {
-                for (int y = 0; y < S[x].Count; y++)
-                {
-                    A[x, y] = S[x][y];
-                }
-            }*/
-            /*
-               CultureInfo ci = new CultureInfo("en-US");
-                using (StreamWriter sw = new StreamWriter("cluster7.txt"))
-                {
-                    for (int m = 0; m < A.RowCount; m++)
-                    {
-                        for (int n = 0; n < A.ColumnCount; n++)
-                        {
-
-                            sw.Write(String.Format(ci, "{0:0.00},", A[m, n]));
-                        }
-                        sw.Write("\n");
-                    }
-                }*/
-            //Matrix<double> A = Matrix<double>.Build.Dense(notAdjacency.ColumnCount, notAdjacency.RowCount);
-            //foreach(var item in itemList)
-            //{
-            //    foreach(var item2 in itemList)
-            //    {
-            //        int res = 0;
-            //        if((item.x == item2.x && item.y == item2.y) || (item.value == 0 || item2.value == 0))
-            //        {
-            //            res = 0;
-            //        }
-            //        else
-            //        {
-            //            if(item.value == 1 && item2.value == 1)
-            //            {
-            //                res = 1;
-            //            }
-            //            //res = ;//Math.Sqrt(Math.Pow(item.x - item2.x, 2) + Math.Pow(item.y - item2.y, 2));
-            //            A[item.x, item.y] = res;
-            //            item.value = res;
-            //            item2.value = res;
-            //        }
-            //        
-            //    }
-            //}
-
 
             Vector<double> dVector = A.RowAbsoluteSums();
             Matrix<double> D = Matrix<double>.Build.DenseOfDiagonalVector(dVector);
@@ -149,7 +87,6 @@ namespace SpectralClustering
             var sortedItemList = lp.OrderBy(x => x.Eigen).ToList();
             Console.WriteLine("Cutting communities.");
             Cut(sortedItemList);
-            //sortedItemList = sortedItemList.OrderBy(x => x.x).ToList();
             Matrix<double> A2 = Matrix<double>.Build.Dense(A.ColumnCount, A.RowCount);
             
             for (int m = 0; m < A2.RowCount; m++)
@@ -165,34 +102,27 @@ namespace SpectralClustering
             WriteImage(A, false, "original");
 
             Bitmap bm = new Bitmap(img);
-            /*for(int i = 0; i < bm.Width; i++)
-            {
-                for (int j = 0; j < bm.Height; j++)
-                {
-                    bm.SetPixel(i, j, Color.Gray);
-                }
-            }*/
             foreach(var v in lp)
             {
                 bm.SetPixel(v.x, v.y, v.clusternum == 1 ? Color.Blue : v.clusternum == 2 ? Color.Red : Color.Black);
             }
             bm.Save("bm.png");
 
-                    //if (cutCommunities == null)
-                    //{
-                    //    return new List<List<User>>() { userList };
-                    //}
-                    //
-                    //List<List<User>> allCommunities = new List<List<User>>();
-                    //foreach (var x in FindCommunities(cutCommunities.Item1))
-                    //{
-                    //    allCommunities.Add(x);
-                    //}
-                    //foreach (var x in FindCommunities(cutCommunities.Item2))
-                    //{
-                    //    allCommunities.Add(x);
-                    //}
-                    //return allCommunities;
+            //if (cutCommunities == null)
+            //{
+            //    return new List<List<User>>() { userList };
+            //}
+            //
+            //List<List<User>> allCommunities = new List<List<User>>();
+            //foreach (var x in FindCommunities(cutCommunities.Item1))
+            //{
+            //    allCommunities.Add(x);
+            //}
+            //foreach (var x in FindCommunities(cutCommunities.Item2))
+            //{
+            //    allCommunities.Add(x);
+            //}
+            //return allCommunities;
         }
 
         public static void WriteImage(Matrix<double> matrix, bool special = false, string filename = "matrix")
